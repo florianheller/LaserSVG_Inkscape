5ad9e317d8bc0604ebdd8243b3885b3a20770aaf
Florian Heller <florian.heller@uhasselt.be>
2020-07-10 00:00:53 +0200

    Initial commit of the control module

---
+++ laserSVG_control.py
@@ -0,0 +1,117 @@
+ #!/usr/bin/env python
+# coding=utf-8
+#
+# Copyright (C) 2020 Florian Heller, florian.heller@uhasselt.be
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+
+# see https://launchpadlibrarian.net/235367843/debug_sel_nodes.py for more details
+import math
+
+import inkex
+from lxml import etree
+
+class LaserSVG(inkex.EffectExtension):
+
+    selected_nodes = {}
+
+    """Replace the selection's nodes with numbered dots according to the options"""
+    def add_arguments(self, pars):
+        pars.add_argument("--material_thickness", default=3, help="The material thickness")
+        pars.add_argument("--tab", help="The selected UI-tab when OK was pressed")
+
+    def effect(self):
+        # self.document and self.svg seem both to be the same etree
+        # The trailing slash is important, otherwise inkscape doesn't load the file correctly
+        LASER_NAMESPACE = "http://www.heller-web.net/lasersvg/"
+        LASER_PREFIX = "laser"
+        LASER = "{%s}" % LASER_NAMESPACE
+        # Register the namespace prefix both with etree and inkscape
+        etree.register_namespace("laser", LASER_NAMESPACE)
+        inkex.utils.NSS["laser"] = LASER_NAMESPACE
+        #inkex.utils.NSS["laser"] = "http://www.heller-web.net/lasersvg"
+        #self.document.register_namespace("laser","http://www.heller-web.net/lasersvg")
+        # inkex.utils.debug(self.document.getroot().nsmap)
+
+        
+        # inkex.utils.debug(inkex.utils.NSS)
+        # inkex.utils.debug(self.document.getroot().nsmap)
+        # inkex.utils.debug(inkex.addNS("thickness-adjust", "laser"))
+        # inkex.utils.debug(self.options.material_thickness)
+
+        self.document.getroot().set(LASER+"material-thickness", self.options.material_thickness)
+        # inkex.utils.debug(self.document.getroot().nsmap)
+        #inkex.utils.NSS["laser"] = "http://www.heller-web.net/lasersvg"
+
+
+        #inkex.utils.debug(etree.tostring(self.document.getroot(),pretty_print=True))
+        rect = self.svg.getElementById('rect214')
+        # inkex.utils.debug(etree.tostring(rect))
+        # inkex.utils.debug(inkex.addNS("thickness-adjust", "laser"))
+        rect.set(inkex.addNS("thickness-adjust", LASER_PREFIX),"width")
+        # #rect.set(XHTML + "thickness-adjust","width")
+        # inkex.utils.debug(etree.tostring(rect))
+
+        # inkex.NSS["laser"] = XHTML_NAMESPACE
+        #inkex.addNS(self.svg, 'laserSVG')
+
+        #for element in self.svg:
+         #   inkex.utils.debug('{0}'.format(element))
+
+
+        # # If nothing is selected, we can't do anything
+        # if not self.svg.selected:
+        #     raise inkex.AbortExtension("Please select an object.")
+
+        # self.selected_nodes = self.parse_selected_nodes(self.options.selected_nodes)
+        
+        # # inkex.utils.debug(self.selected_nodes)
+
+        # # report about selected nodes
+        # for pathID, selected in self.selected_nodes.items():
+        #     path = self.svg.getElementById(pathID)
+        #     # p2 = path.original_path.to_superpath()
+        #     # inkex.utils.debug('{0}'.format(p2))
+        #     for element in selected: 
+        #         inkex.utils.debug( '{0} - selected nodes of subpath {1}: {2}'.format(pathID, element, selected[element]))
+        #         # show the coordinates
+        #         for nodes in selected[element]:
+        #             coordinates = path.original_path.to_superpath()[element][nodes][1]
+        #             inkex.utils.debug('{0} - selected node {1}: {2}'.format(pathID, nodes, coordinates))
+        #         # Check if path segment length matches specified thickness
+        #         # check if a path template is already defined, if not, create one
+        #         # place the {thickness} label at the repective place in the path template
+
+
+    def parse_selected_nodes(self, nodes):
+        result = {}
+        for elem in nodes:
+            # inkex.utils.debug(elem)
+            elemData = elem.rsplit(':', 2)
+            pathID = elemData[0]
+            sub_path = int(elemData[1])
+            sel_node = int(elemData[2])
+            if pathID not in result:
+                result[pathID] = {sub_path: [sel_node]}
+            else:
+                if sub_path not in result[pathID]:
+                    result[pathID][sub_path] = [sel_node]
+                else:
+                    result[pathID][sub_path].extend([sel_node])
+        return result
+
+if __name__ == '__main__':
+    LaserSVG().run()
