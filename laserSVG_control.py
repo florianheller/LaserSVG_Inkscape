bde2c94567015d8ac853485036a3176ecf39478e
Florian Heller <florian.heller@uhasselt.be>
2020-07-14 09:27:20 +0200

    Removed test code

--- laserSVG_control.py
+++ laserSVG_control.py
@@ -18,7 +18,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
 
-# see https://launchpadlibrarian.net/235367843/debug_sel_nodes.py for more details
+
 import math
 
 import inkex
@@ -31,7 +31,6 @@ class LaserSVG(inkex.EffectExtension):
     LASER_PREFIX = "laser"
     LASER = "{%s}" % LASER_NAMESPACE
 
-    """Replace the selection's nodes with numbered dots according to the options"""
     def add_arguments(self, pars):
         pars.add_argument("--material_thickness", default=3, help="The material thickness")
         pars.add_argument("--tab", help="The selected UI-tab when OK was pressed")
@@ -56,42 +55,9 @@ class LaserSVG(inkex.EffectExtension):
 
 
         #inkex.utils.debug(etree.tostring(self.document.getroot(),pretty_print=True))
-        rect = self.svg.getElementById('rect214')
-        # inkex.utils.debug(etree.tostring(rect))
-        # inkex.utils.debug(inkex.addNS("thickness-adjust", "laser"))
-        rect.set(inkex.addNS("thickness-adjust", self.LASER_PREFIX),"width")
+
         # #rect.set(XHTML + "thickness-adjust","width")
         # inkex.utils.debug(etree.tostring(rect))
-
-
-
-
-        #for element in self.svg:
-         #   inkex.utils.debug('{0}'.format(element))
-
-
-        # # If nothing is selected, we can't do anything
-        # if not self.svg.selected:
-        #     raise inkex.AbortExtension("Please select an object.")
-
-        # self.selected_nodes = self.parse_selected_nodes(self.options.selected_nodes)
-        
-        # # inkex.utils.debug(self.selected_nodes)
-
-        # # report about selected nodes
-        # for pathID, selected in self.selected_nodes.items():
-        #     path = self.svg.getElementById(pathID)
-        #     # p2 = path.original_path.to_superpath()
-        #     # inkex.utils.debug('{0}'.format(p2))
-        #     for element in selected: 
-        #         inkex.utils.debug( '{0} - selected nodes of subpath {1}: {2}'.format(pathID, element, selected[element]))
-        #         # show the coordinates
-        #         for nodes in selected[element]:
-        #             coordinates = path.original_path.to_superpath()[element][nodes][1]
-        #             inkex.utils.debug('{0} - selected node {1}: {2}'.format(pathID, nodes, coordinates))
-        #         # Check if path segment length matches specified thickness
-        #         # check if a path template is already defined, if not, create one
-        #         # place the {thickness} label at the repective place in the path template
 
     def adjust_element_thickness(self, newThickness):
         for node in self.document.getroot().iterfind(".//*[@%sthickness-adjust]" % self.LASER):
@@ -105,25 +71,9 @@ class LaserSVG(inkex.EffectExtension):
                 node.attrib["width"] = newThickness
             # inkex.utils.debug(node.get("laser:thickness-adjust"))    
             # inkex.utils.debug(node.attrib)    
-        # nodes = self.document.getroot().findall(".//*[@%s:thickness-adjust]" % self.LASER_PREFIX)
-        
+            # nodes = self.document.getroot().findall(".//*[@%s:thickness-adjust]" % self.LASER_PREFIX)
+ 
 
-    def parse_selected_nodes(self, nodes):
-        result = {}
-        for elem in nodes:
-            # inkex.utils.debug(elem)
-            elemData = elem.rsplit(':', 2)
-            pathID = elemData[0]
-            sub_path = int(elemData[1])
-            sel_node = int(elemData[2])
-            if pathID not in result:
-                result[pathID] = {sub_path: [sel_node]}
-            else:
-                if sub_path not in result[pathID]:
-                    result[pathID][sub_path] = [sel_node]
-                else:
-                    result[pathID][sub_path].extend([sel_node])
-        return result
 
 if __name__ == '__main__':
     LaserSVG().run()
