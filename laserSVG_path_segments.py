f16320aa49f5cd6208417bd5e750513c74147116
Florian Heller <florian.heller@uhasselt.be>
2020-09-22 14:57:30 +0200

    Correct check wether base and gap are parallel

--- laserSVG_path_segments.py
+++ laserSVG_path_segments.py
@@ -284,12 +284,11 @@ class LaserSVG(inkex.EffectExtension):
 
 
                 # If the segment leading to or from the slit is parallel to the slit base, we do not need to adjust the length of the slit walls
-                
-                if (ll != None and (abs(ll_angle) - abs(center.angle)) > 0.0001) or self.options.assume_parallel == "false":
+                if (ll != None and (abs(ll_angle - center.angle)) > 0.0001) or self.options.assume_parallel == "false":
                     calc_l = self.shortenSlitLeg(thickness, gap, template[index], template[index-1], l)
                     template[index-1] = self.tagCommandWithCalculation(template[index-1],calc_l)
 
-                if (rr != None and (abs(rr.angle) - abs(center.angle)) > 0.0001) or self.options.assume_parallel == "false" :
+                if (rr != None and (abs(rr.angle - center.angle)) > 0.0001) or self.options.assume_parallel == "false" :
                     calc_r = self.shortenSlitLeg(thickness, gap, template[index], template[index+1], r)
                     template[index+1] = self.tagCommandWithCalculation(template[index+1],calc_r)
 
